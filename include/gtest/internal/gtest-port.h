c0b9b58b972500e4228034e0e038e165b51f06d4
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2012-05-12 23:48:12 +0100

    Make Clang ignore this warning, as per the comment.

--- include/gtest/internal/gtest-port.h
+++ include/gtest/internal/gtest-port.h
@@ -1380,6 +1380,7 @@ class MutexBase {
 # define GTEST_DECLARE_STATIC_MUTEX_(mutex) \
     extern ::testing::internal::MutexBase mutex
 
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 // Defines and statically (i.e. at link time) initializes a static mutex.
 // The initialization list here does not explicitly initialize each field,
 // instead relying on default initialization for the unspecified fields. In
